10f46588f80c05d2907b90e7a46b26b27a195b60
Mrxmm <362190221@qq.com>
2021-06-20 22:28:45 +0800

    结构体和方法

--- 4.OO/4.1.struct.go
+++ 4.OO/4.1.struct.go
@@ -12,16 +12,48 @@ func initStruct() {
 	// 方式一
 	var root TreeNode
 
-	root = TreeNode{Value:3}
+	root = TreeNode{Value:2}
 	root.Left = &TreeNode{} // Left 是指针，所以传递给Left是变量地址，需要加&号
-	root.Right = &TreeNode{nil, nil, 5}
+	root.Left.Right = createNode(3)
+	root.Right = &TreeNode{nil, nil, 4}
 	root.Right.Left = new(TreeNode)
+	root.Right.Left.setValue2(5)
 
-	nodes := []TreeNode {
-		{Value :  5},
-		{nil, &root, 6},
+	root.traverse()
+}
+
+// 工厂函数
+func createNode(value int) *TreeNode {
+	return &TreeNode{Value: value} // 返回局部变量的地址 给全局使用 C++中程序会挂
+}
+
+// (node TreeNode) - 接收者
+func (node TreeNode) print() {
+	fmt.Print(node.Value)
+}
+
+func print(node TreeNode) {
+	fmt.Print(node.Value)
+}
+
+// 值传递
+func (node TreeNode) setValue1 (value int) {
+	node.Value = value
+}
+
+// 引用传递
+func (node *TreeNode) setValue2 (value int) {
+	node.Value = value
+}
+
+// 中序遍历
+func (node *TreeNode) traverse() {
+	if (node == nil) {
+		return
 	}
-	fmt.Println(nodes)
+	node.Left.traverse()
+	node.print()
+	node.Right.traverse()
 }
 
 func main() {
