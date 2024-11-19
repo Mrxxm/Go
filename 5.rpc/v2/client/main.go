8187dc2354720df00525fcb7092a4633215f3113
Mrxxm <362190221@qq.com>
2024-11-19 16:03:28 +0800

    使用rpc内置库实现

--- 5.rpc/v2/client/main.go
+++ 5.rpc/v2/client/main.go
@@ -1,1 +1,21 @@
-package client
+package main
+
+import (
+	"fmt"
+	"net/rpc"
+)
+
+func main() {
+	// 1.建立连接
+	client, err := rpc.Dial("tcp", "localhost:1234")
+	if err != nil {
+		panic("连接失败")
+	}
+	//var reply *string = new(string)
+	var reply string // string有默认值
+	err = client.Call("HelloService.Hello", "xxm", &reply)
+	if err != nil {
+		panic("调用失败")
+	}
+	fmt.Println(reply)
+}
--- 5.rpc/v2/server/main.go
+++ 5.rpc/v2/server/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"net"
+	"net/rpc"
+)
+
+type HelloService struct{}
+
+func (s *HelloService) Hello(request string, reply *string) error {
+	// 返回值是通过修改reply的值
+	*reply = "hello, " + request
+
+	return nil
+}
+
+func main() {
+	// 1.实例化一个server
+	listener, _ := net.Listen("tcp", ":1234")
+	// 2.注册处理逻辑handler
+	_ = rpc.RegisterName("HelloService", &HelloService{})
+	// 3.启动服务
+	conn, _ := listener.Accept() // 当一个新的连接进来的时候
+
+	rpc.ServeConn(conn) // 只能处理一次连接
+
+	// rpc调用中解决了两个问题：1.call id 2.序列化和反序列化
+}
