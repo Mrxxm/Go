19eb198b640a9ed57724d9f77b11e487de5a8169
Mrxxm <362190221@qq.com>
2024-11-20 11:08:55 +0800

    添加注释

--- 5.rpc/v5/handler/handler.go
+++ 5.rpc/v5/handler/handler.go
@@ -4,6 +4,7 @@ const HelloServiceName = "handler/HelloService"
 
 type HelloService struct{}
 
+// 业务逻辑
 func (s *HelloService) Hello(request string, reply *string) error {
 	// 返回值是通过修改reply的值
 	*reply = "hello, " + request
