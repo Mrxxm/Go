aada3af7f1e4acb1a9ecd5079b76f79651a9dce8
Mrxxm <362190221@qq.com>
2024-12-10 16:13:27 +0800

    返回json或protobuf

---
+++ 7_json_protobuf/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"Go/7_json_protobuf/protobuf"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	routeGroup := router.Group("/gin_test")
+	{
+		routeGroup.GET("/more_json", moreJson)
+		routeGroup.GET("/some_protobuf", someProtobuf)
+
+	}
+
+	router.Run(":8081")
+}
+
+func someProtobuf(context *gin.Context) {
+	teacher := &protobuf.Teacher{
+		Name:   "xxm",
+		Course: []string{"math", "english"},
+	}
+
+	context.ProtoBuf(http.StatusOK, teacher)
+}
+
+func moreJson(context *gin.Context) {
+	var message struct {
+		Name    string `json:"user"`
+		Message string
+		Number  int
+	}
+	message.Name = "xxm"
+	message.Message = "hello"
+	message.Number = 123
+
+	context.JSON(http.StatusOK, message)
+}
