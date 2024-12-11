1e32b3d4829e17b4f2df3cd2e0079ae785cffb3b
Mrxxm <362190221@qq.com>
2024-12-11 14:54:08 +0800

    返回字符串调整

--- 7_gin/validate_translate_error/main.go
+++ 7_gin/validate_translate_error/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"reflect"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
@@ -23,6 +25,15 @@ var trans ut.Translator
 func InitTrans(locale string) (err error) {
 	// 修改gin框架中的Validator引擎属性，实现自定制
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+
+		// 注册一个获取json tag的自定义方法
+		v.RegisterTagNameFunc(func(fld reflect.StructField) string {
+			name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+			if name == "-" {
+				return ""
+			}
+			return name
+		})
 
 		zhT := zh.New() // 中文翻译器
 		enT := en.New() // 英文翻译器
