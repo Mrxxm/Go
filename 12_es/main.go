69772ecb5bc210e9bdbc0e67da9e75cf447a2c51
Mrxxm <362190221@qq.com>
2025-03-25 11:20:46 +0800

    添加数据

--- 12_es/main.go
+++ 12_es/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"github.com/olivere/elastic/v7"
 )
@@ -21,28 +20,35 @@ func main() {
 		panic(err)
 	}
 
-	q := elastic.NewMatchQuery("address", "street")
+	// 1.查询数据
+	//q := elastic.NewMatchQuery("address", "street")
+	//result, err := client.Search().Index("user").Query(q).Do(context.Background())
+	//if err != nil {
+	//	panic(err)
+	//}
+	//total := result.Hits.TotalHits.Value
+	//fmt.Printf("搜索结果数量：%d/n", total)
+	//for _, value := range result.Hits.Hits {
+	//	// 将字符串转成struct对象
+	//	account := Account{}
+	//	_ = json.Unmarshal(value.Source, &account)
+	//	fmt.Println(account)
 
-	result, err := client.Search().Index("user").Query(q).Do(context.Background())
+	//if jsonData, err := value.Source.MarshalJSON(); err == nil {
+	//	fmt.Println(string(jsonData))
+	//}
+	//}
 
+	// 2.添加数据
+	acc := Account{AccountNumber: 14268, FirstName: "xxm"}
+	put1, err := client.Index().
+		Index("account").
+		BodyJson(acc).
+		Do(context.Background())
 	if err != nil {
 		panic(err)
 	}
-
-	total := result.Hits.TotalHits.Value
-	fmt.Printf("搜索结果数量：%d/n", total)
-	for _, value := range result.Hits.Hits {
-
-		// 将字符串转成struct对象
-		account := Account{}
-		_ = json.Unmarshal(value.Source, &account)
-		fmt.Println(account)
-
-		//if jsonData, err := value.Source.MarshalJSON(); err == nil {
-		//	fmt.Println(string(jsonData))
-		//}
-	}
-
+	fmt.Printf("Indexed account %s to index %s, type %s\n", put1.Id, put1.Index, put1.Type)
 	// Use the client
 	_ = client
 }
